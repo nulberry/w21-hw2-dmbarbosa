9220567f550ed7f85d814aba7317543891d431ad
Denilson Barbosa <denilson@ualberta.ca>
2021-02-10 09:11:07 -0700

    Update solution.c

--- q2/src/solution.c
+++ q2/src/solution.c
@@ -2,9 +2,14 @@
 #include <solution.h>
 #include <stdlib.h>
 
+  void f(void)
+  {
+     int* x = malloc(10 * sizeof(int));
+     x[10] = 0;        // problem 1: heap block overrun
+  }                    // problem 2: memory leak -- x not freed
+
 int main() {
-  int* x = malloc(10 * sizeof(int));
-  x[10] = 0;
+  f();  
   
   printf("0.0\n");
   return(0);
