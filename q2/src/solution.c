5d99c4f5bebfd6ceda87770ebbf66cad501fa931
Denilson Barbosa <denilson@ualberta.ca>
2021-02-10 09:14:00 -0700

    Update solution.c

--- q2/src/solution.c
+++ q2/src/solution.c
@@ -5,7 +5,7 @@
   void f(void)
   {
      int* x = malloc(10 * sizeof(int));
-     x[10] = 0;        // problem 1: heap block overrun
+//      x[10] = 0;        // problem 1: heap block overrun
   }                    // problem 2: memory leak -- x not freed
 
 int main() {
